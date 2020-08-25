e89aa934fef0198c1c617371ff29dccb6b318a0f
Anthony Loop <anthony@anthonyloop.com>
2020-08-25 03:57:04 -0700

    Specify HUGO_ENV in build script

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@ set -euo pipefail
 
 shopt -s globstar
 
-if hugo --gc --minify --cleanDestinationDir=true; then
+if HUGO_ENV=production hugo --gc --minify --cleanDestinationDir=true; then
   # Try to compress the files ahead of time so the webserver can do less work
   for uncompressed_file in public/**/*.{html,css,js,mjs,json,svg,xml}; do
     if [ -e "$uncompressed_file" ] && [ ! -d "$uncompressed_file" ]; then
