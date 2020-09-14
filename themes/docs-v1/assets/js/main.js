9904ba2b9cc621c05800d6de76701b8bf307bcbb
Anthony Loop <anthony@anthonyloop.com>
2020-09-13 20:48:59 -0700

    Use button instead of hover to open nav menu

--- themes/docs-v1/assets/js/main.js
+++ themes/docs-v1/assets/js/main.js
@@ -2,3 +2,13 @@
 if ("serviceWorker" in navigator) {
   navigator.serviceWorker.register("/service-worker.js");
 }
+
+// Setup handler for opening navigation
+const navContainer = document.querySelector("#nav-container");
+const toggle = document.querySelector("#nav-toggle");
+
+if (navContainer !== null && toggle !== null) {
+  toggle.addEventListener("click", () => {
+    navContainer.classList.toggle("is-open");
+  });
+}
