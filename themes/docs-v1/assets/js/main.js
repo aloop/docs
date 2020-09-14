89a10c39682d9b46e48f3885708fcc480dec6b51
Anthony Loop <anthony@anthonyloop.com>
2020-09-14 03:12:11 -0700

    Close nav on viewport resize

--- themes/docs-v1/assets/js/main.js
+++ themes/docs-v1/assets/js/main.js
@@ -3,27 +3,44 @@ if ("serviceWorker" in navigator) {
   navigator.serviceWorker.register("/service-worker.js");
 }
 
-// Setup handler for opening navigation
+// Setup handlers for opening and closing navigation
 const navContainer = document.querySelector("#nav-container");
 const toggle = document.querySelector("#nav-toggle");
 
 if (navContainer !== null && toggle !== null) {
-  toggle.addEventListener("click", () => {
-    navContainer.classList.toggle("is-open");
-  });
+  const openClassName = "is-open";
 
-  document.addEventListener("keyup", ({ key }) => {
+  const hasClass = () => navContainer.classList.contains(openClassName);
+  const removeClass = () => navContainer.classList.remove(openClassName);
+
+  const closeNav = () => {
+    if (hasClass()) {
+      removeClass();
+    }
+  };
+
+  const handleEscape = ({ key }) => {
     if (key === "Escape") {
-      navContainer.classList.remove("is-open");
+      closeNav();
+    }
+  };
+
+  const closeOnClick = ({ target }) => {
+    if (hasClass() && !navContainer.contains(target)) {
+      removeClass();
+    }
+  };
+
+  resizeOpts = { once: true };
+
+  toggle.addEventListener("click", async () => {
+    navContainer.classList.toggle(openClassName);
+
+    if (navContainer.classList.contains(openClassName)) {
+      window.addEventListener("resize", closeNav, resizeOpts);
     }
   });
 
-  document.addEventListener("click", ({ target }) => {
-    if (
-      navContainer.classList.contains("is-open") &&
-      !navContainer.contains(target)
-    ) {
-      navContainer.classList.remove("is-open");
-    }
-  });
+  document.addEventListener("keyup", handleEscape);
+  document.addEventListener("click", closeOnClick);
 }
