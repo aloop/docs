bb7891ae7f87cb404da03ddc734111451f2dccea
Anthony Loop <anthony@anthonyloop.com>
2023-06-24 14:50:03 -0700

    Remove service worker/offline support

--- themes/docs-v1/assets/js/offline-inline.js
+++
@@ -1,7 +0,0 @@
-window.addEventListener("load", () => {
-  window.addEventListener("online", () => {
-    if (window.location.pathname !== "/offline.html") {
-      window.location.reload();
-    }
-  });
-});
