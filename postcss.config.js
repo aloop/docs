c6d7719166a64057e0b415a25829aed186e00749
Anthony Loop <anthony@anthonyloop.com>
2023-10-15 16:28:20 -0700

    update deps, add purgecss

--- postcss.config.js
+++ postcss.config.js
@@ -1,3 +1,19 @@
+const purgecss = require("@fullhuman/postcss-purgecss");
+
+const defaultExtractor = (content) => content.match(/[\w-/.:@]+(?<!:)/g) || [];
+
 module.exports = {
-  plugins: [require("autoprefixer")],
+  plugins: [
+    purgecss({
+      content: ["themes/docs-v1/**/*.{js,html}", "content/**/*.{html,md}"],
+      extractors: [
+        {
+          extractor: defaultExtractor,
+          extensions: ["js", "html", "md"],
+        },
+      ],
+      safelist: [/^(is-|has-|will-|js-)/],
+    }),
+    require("autoprefixer"),
+  ],
 };
