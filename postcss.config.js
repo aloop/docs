61d0293fc7ef68e0786754ef879b31e9e96e2282
Anthony Loop <anthony@anthonyloop.com>
2023-10-15 16:50:23 -0700

    cleanup postcss config, use dart-sass

--- postcss.config.js
+++ postcss.config.js
@@ -2,18 +2,22 @@ const purgecss = require("@fullhuman/postcss-purgecss");
 
 const defaultExtractor = (content) => content.match(/[\w-/.:@]+(?<!:)/g) || [];
 
+const purgecssConfig = {
+  content: ["themes/docs-v1/**/*.{js,html}", "content/**/*.{html,md}"],
+  extractors: [
+    {
+      extractor: defaultExtractor,
+      extensions: ["js", "html", "md"],
+    },
+  ],
+  safelist: [/^(is-|has-|will-|js-)/],
+};
+
 module.exports = {
   plugins: [
-    purgecss({
-      content: ["themes/docs-v1/**/*.{js,html}", "content/**/*.{html,md}"],
-      extractors: [
-        {
-          extractor: defaultExtractor,
-          extensions: ["js", "html", "md"],
-        },
-      ],
-      safelist: [/^(is-|has-|will-|js-)/],
-    }),
     require("autoprefixer"),
+    ...(process.env.HUGO_ENVIRONMENT === "production"
+      ? [purgecss(purgecssConfig)]
+      : []),
   ],
 };
